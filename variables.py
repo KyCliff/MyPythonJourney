db5cac3069e8898c26c393fdb8efb826aaf7011b
Kyle Clifton <k.clifton2004@gmail.com>
2025-09-27 10:19:55 -0400

    Added variables practice code from Module 2

---
+++ variables.py
@@ -0,0 +1,13 @@
+# This program demonstrates basic variables/data types.
+
+# Declaring each variable with name and appropriate data type
+name = "Kyle Clifton"       # String
+age = 39                    # Integer
+height = 1.85               # Float
+liking_python = True        # Boolean
+
+# Printing each variable
+print("Name:", name)                        # Should output Name: Kyle Clifton
+print("Age:", age)                          # Should output Age: 39
+print("Height:", height, "m")               # Should output Height: 1.85 m
+print("Liking Python?:", liking_python)     # Should output Liking Python?: True
